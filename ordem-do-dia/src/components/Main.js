5a1fd3b4ae7f06e0cad561fd3dca8e78d7b2e1ca
Vitor Sandes <sandesvitor@gmail.com>
2020-05-16 12:08:24 -0300

    Updated components

--- ordem-do-dia/src/components/Main.js
+++ ordem-do-dia/src/components/Main.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 
 export class Main extends Component {
-    state = {}
+    constructor(props){
+        super(props)
 
-    myFunction() {}
+        this.state = {}
+
+        this.myFunction = this.myFunction.bind(this)
+    }
+
+    myFunction() {
+        console.log('Funcioando!')
+        }
+    
 
     render() {
         return (
@@ -11,7 +20,7 @@ export class Main extends Component {
                 <p>
                     <h1>SUA PRODUÇÃO MAIS EFICIENTE</h1>
                     <h2>ORDEM DO DIA NA PALMA DA SUA MÃO</h2>
-                    <input type='button' id="btn-main" value="COMECE AGORA!" onClick='myFunction()'></input>
+                    <input type='button' id="btn-main" value="COMECE AGORA!" onClick = {this.myFunction}></input>
                 </p>
                 
             </div>
--- ordem-do-dia/src/components/Rodape.js
+++ ordem-do-dia/src/components/Rodape.js
@@ -6,6 +6,36 @@ export class Rodape extends Component {
     render() {
         return (
             <div className="rodape-section">
+                <div className="rodape-infos">
+                    <ul>
+                        <li>
+                            <p>
+                                <h1>CONTATOS</h1>
+                                <h2>+55 21 99999999<br/>sandes@ordemdodia.com.br<br/>Tinder</h2>
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <h1>SERVIÇOS</h1>
+                                <h2>Entre em Contato<br/>Assine um Plano<br/>Dúvidas</h2>
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <h1>INFORMAÇÕES</h1>
+                                <h2>Trabalhe Conosco<br/>Política de Privacidade<br/>Termos & Condições</h2>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="rodape-icons">
+                    <a href="https:/www.instagram.com/"><img src="./instagram-logo.png" id="instagram-logo"></img></a>
+                    <a href="https:/twitter.com/"><img src="./twitter-logo.png" id="twitter-logo"></img></a>
+                    <a href="https:/www.facebook.com/"><img src="./facebook-logo.png" id="facebook-logo"></img></a>
+                </div>
+                <span className="rodape-brand">
+                © ORDEM DO DIA 2020
+                </span>
             </div>
         )
     }
